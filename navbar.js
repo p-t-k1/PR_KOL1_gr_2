a1c39e3583782de2c59b0ae452eec01b32bae17f
Patryk Przybyło <patryk13342@o2.pl>
2021-04-09 12:14:47 +0200

    kolokwium

---
+++ navbar.js
@@ -0,0 +1,3 @@
+let nav = '<nav> <h1>Nawigacja</h1><a href="index.html">Główna</a> <a href="kontakt.html">Kontakt</a> <a href="formularz.html">Formularz</a></nav>'
+
+document.querySelector('.nav-bar').innerHTML = nav;
---
+++ script2.js
@@ -0,0 +1,8 @@
+
+let button2 = document.querySelector('.button2');
+
+
+
+button2.addEventListener('click', function() {
+	document.body.style.backgroundColor = "red";
+});
